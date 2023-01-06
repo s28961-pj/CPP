e5a2d90bfd46cc1200eef256d9c3a622a49dac67
Roland Liedtke <roland-liedtke@Rolands-MacBook-Air.local>
2023-01-06 18:21:57 +0100

    dodaje rozwiazanie zadanie04

--- PPRG_02c/Zadanie04/zadanie04.cpp
+++ PPRG_02c/Zadanie04/zadanie04.cpp
@@ -2,16 +2,22 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     int n = 0;
     int suma = 0;
 
+    cout << "\nSUMA KWADRATOW OD 1 DO n\n"
+         << endl;
+    cout << "------------------" << endl;
+
     cout << "Wprowadz n: ";
     cin >> n;
-    for (int i=0; i <= n; i++) {
-        suma += i*i;
+    for (int i = 0; i <= n; i++)
+    {
+        suma += i * i;
     }
-    cout << "Sumę kdwadratów liczb od 1 do n\n(1*1 + 2*2 + ... + n*n)= " << suma << endl;
+    cout << "Sumę kdwadratów liczb od 1 do " << n << " wynosi " << suma << endl;
 
     return 0;
 }
